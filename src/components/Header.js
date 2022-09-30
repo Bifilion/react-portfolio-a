d6191d85de4e1cea265e9ee5ccefd32c657b177c
Bifilion <jiriplic@seznam.cz>
2022-09-30 09:32:56 +0200

    created react app, components folder

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import Nav from "./Nav";
+import NavMobile from "./NavMobile";
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
